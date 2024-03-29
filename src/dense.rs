10cad84613a15c116f37980d12af418fb6eacde5
Yosuke Ide <gizaneko@outlook.jp>
2024-03-30 00:34:41 +0900

    Added systolic emulation

--- src/dense.rs
+++ src/dense.rs
@@ -5,22 +5,122 @@ pub fn mv_dense(mat_a: &Array2<f64>, vec_x: &Array1<f64>, vec_y: &mut Array1<f64
     let shape_a = mat_a.shape();
     let shape_x = vec_x.shape();
     let shape_y = vec_y.shape();
-    let col = shape_a[1];
-    let row = shape_a[0];
-    if col != shape_x[0] {
+    if shape_a[1] != shape_x[0] {
         println!("vec_x shape erorr");
         std::process::exit(1);
     }
-    if row != shape_y[0] {
+    if shape_a[0] != shape_y[0] {
         println!("vec_y shape erorr");
         std::process::exit(1);
     }
 
-    for i in 0..row {
+    for i in 0..shape_a[0] {
         let mut sum : f64 = 0.0;
-        for j in 0..col {
+        for j in 0..shape_a[1] {
             sum = sum + mat_a[[i, j]] * vec_x[[j]];
         }
         vec_y[[i]] = sum;
+    }
+}
+
+pub fn mm_dense(mat_a: &Array2<f64>, mat_b: &Array2<f64>, mat_z: &mut Array2<f64>) {
+    let shape_a = mat_a.shape();
+    let shape_b = mat_b.shape();
+    let shape_z = mat_z.shape();
+
+    if shape_a[1] != shape_b[0] {
+        println!("mat_b shape error");
+        std::process::exit(1);
+    }
+    if shape_a[0] != shape_z[0] {
+        println!("mat_z shape error");
+        std::process::exit(1);
+    }
+    if shape_b[1] != shape_z[1] {
+        println!("mat_z shape error");
+        std::process::exit(1);
+    }
+
+    for i in 0..shape_b[1] {
+        for j in 0..shape_a[0] {
+            let mut sum : f64 = 0.0;
+            for k in 0..shape_a[1] {
+                sum = sum + mat_a[[j, k]] * mat_b[[k,i]];
+            }
+            mat_z[[j,i]] = sum;
+        }
+    }
+}
+
+// Non-stationary systolic array (NSA) emulation
+pub fn mm_systolic_nsa(mat_a: &Array2<f64>, mat_b: &Array2<f64>, mat_z: &mut Array2<f64>) {
+    let shape_a = mat_a.shape();
+    let shape_b = mat_b.shape();
+    let shape_z = mat_z.shape();
+
+    if shape_a[1] != shape_b[0] {
+        println!("mat_b shape error");
+        std::process::exit(1);
+    }
+    if shape_a[0] != shape_z[0] {
+        println!("mat_z shape error");
+        std::process::exit(1);
+    }
+    if shape_b[1] != shape_z[1] {
+        println!("mat_z shape error");
+        std::process::exit(1);
+    }
+
+    let mut ffd_x: Array2<f64> = Array::zeros((shape_a[0], shape_b[1]));
+    let mut ffd_y: Array2<f64> = Array::zeros((shape_a[0], shape_b[1]));
+    let mut ffq_x  : Array2<f64> = Array::zeros((shape_a[0], shape_b[1]));
+    let mut ffq_y  : Array2<f64> = Array::zeros((shape_a[0], shape_b[1]));
+
+    let cycles = (shape_a[0]-1) + shape_a[1] + (shape_b[1]-1);
+
+    for i in 0..cycles {
+        for j in 0..shape_a[0] {
+            for k in 0..shape_b[1] {
+                // x direction
+                if k == 0 {
+                    if ( i < j ) || ( shape_b[1] < i + 1 - j ) {
+                        ffq_x[[j,k]] = 0.0;
+                    } else {
+                        let x_idx    = shape_b[1] -1 + j - i;
+                        ffq_x[[j,k]] = mat_a[[j,x_idx]];
+                    }
+                } else {
+                    ffq_x[[j,k]] = ffd_x[[j,k-1]];
+                }
+
+                // y direction
+                if j == 0 {
+                    if ( i < k ) || ( shape_a[0] < i + 1 - k ) {
+                        ffq_y[[j,k]] = 0.0;
+                    } else {
+                        let y_idx    = shape_a[0] -1 + k - i;
+                        ffq_y[[j,k]] = mat_b[[y_idx,k]];
+                    }
+                } else {
+                    ffq_y[[j,k]] = ffd_y[[j-1,k]];
+                }
+            }
+        }
+
+        //println!("iterate: {}", i);
+        //println!("ffq_x: ");
+        //println!("{}", ffq_x);
+        //println!("ffq_y: ");
+        //println!("{}", ffq_y);
+        //println!("");
+
+        for j in 0..shape_a[0] {
+            for k in 0..shape_b[1] {
+                mat_z[[j,k]] = mat_z[[j,k]] + ffq_x[[j,k]] * ffq_y[[j,k]];
+            }
+        }
+
+        ffd_x = ffq_x.clone();
+        ffd_y = ffq_y.clone();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,12 @@ use csr::*;
 use dense::*;
 
 fn main() {
-    let col = 100;
-    let row = 100;
-    let nnz_rate = 0.1;
+    let col = 3;
+    let row = 3;
+    let nnz_rate = 1.0;
     let mut rng = rand::thread_rng();
 
+    //***** Matrix Vector Multiply test
     // Matrix/Vector initialization
     let mat_a: Array2<f64> = random((row, col));
     let spmat_a = mat_a.mapv(|f| if rng.gen::<f64>() < nnz_rate {f} else {0.0});
@@ -36,7 +37,20 @@ fn main() {
     // Matrix Operation
     let mut vec_z: Array1<f64> = Array::zeros(row);
     SpMV_csr(&spmat_a_csr, &vec_x, &mut vec_z);
- 
+
+    // Answer
+    let ans_vec_y = spmat_a.dot(&vec_x);
+
+    // result checks
+    for (i, (a, b)) in vec_y.iter().zip(ans_vec_y.iter()).enumerate() {
+        // Compare result considering rounding error.
+        // Probably "dot" of ndarray_linalg uses Fused Multiply and add (FMA),
+        // which causes precision difference from separeted muliply and add.
+        if (a - b).abs() > 0.000001 {
+            println!("{}: fail", i);
+        }
+    }
+
     println!("Dense -> Sparse -> Dense conversion check");
     if spmat_a != spmat_b {
         println!("  check failed");
@@ -46,6 +60,50 @@ fn main() {
 
     println!("Sparse matrix multiplication check");
     if vec_z != vec_y {
+        println!("  check failed");
+    } else {
+        println!("  check passed");
+    }
+
+
+    //***** Matrix Matrix Multiply test
+    let mat_b: Array2<f64> = random((col, row));
+    let spmat_b = mat_a.mapv(|f| if rng.gen::<f64>() < nnz_rate {f} else {0.0});
+
+    let mut mat_z :Array2<f64> = Array::zeros((row, row));
+
+    let ans_mat_z = spmat_a.dot(&spmat_b);
+
+    mm_dense(&spmat_a, &spmat_b, &mut mat_z);
+
+    let mut mat_z_nsa :Array2<f64> = Array::zeros((row, row));
+    mm_systolic_nsa(&spmat_a, &spmat_b, &mut mat_z_nsa);
+
+    // result checks
+    let mut mm_error = false;
+    println!("Matrix multiply check");
+    for (i, (a, b)) in mat_z.iter().zip(ans_mat_z.iter()).enumerate() {
+        // Compare result considering rounding error.
+        if (a - b).abs() > 0.000001 {
+            mm_error = true;
+        }
+    }
+    if ( mm_error ) {
+        println!("  check failed");
+    } else {
+        println!("  check passed");
+    }
+
+    // result checks (matrix multiply vs systolic)
+    let mut sa_error = false;
+    println!("Systolic matrix multiply check");
+    for (i, (a, b)) in mat_z.iter().zip(mat_z_nsa.iter()).enumerate() {
+        // Compare result considering rounding error.
+        if (a - b).abs() > 0.000001 {
+            sa_error = true;
+        }
+    }
+    if ( sa_error ) {
         println!("  check failed");
     } else {
         println!("  check passed");
